83a5d90f727c6579b2bfbdc6b74a1372f0272423
luckydev8888 <191312059+luckydev8888@users.noreply.github.com>
2024-08-23 07:20:21 +0100

    update by csv file added

--- app/admin/constdf_routes.py
+++ app/admin/constdf_routes.py
@@ -1,6 +1,17 @@
-from flask import render_template, request, Blueprint
+from flask import render_template, request, Blueprint, jsonify
 from app.admin.script.const import get_const, add_const, update_const, delete_const
 from app.admin.main_routes import login_required
+from werkzeug.utils import secure_filename
+from dotenv import load_dotenv
+import os
+import csv
+from app.mongo import get_db
+
+load_dotenv()
+UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER")
+CONSTDF_ID = os.getenv("CONSTDF_ID")
+file_path = ""
+
 
 constdf_bp = Blueprint('constdf', __name__, url_prefix="constdf")
 # constdf page
@@ -43,3 +54,75 @@ def constdf_delete(current_user):
         id = request.form.get('id')
         
         return delete_const(id)
+    
+@constdf_bp.route('/uploadconstdfcsv', methods=['POST'])
+@login_required
+def constdf_upload_constdf_csv(current_user):
+    if request.method == 'POST':
+        if 'constdf_csv_file' not in request.files:
+            return jsonify("no file"), 400
+        file = request.files['constdf_csv_file']
+        if file.filename == '':
+            return jsonify("no file name"), 400
+
+        filename = secure_filename(file.filename)
+
+        
+        global file_path
+        file_path = os.path.join(UPLOAD_FOLDER, filename)
+
+        file.save(file_path)
+        
+        response = {
+            'message' : "successfully uploaded"
+        }
+
+        return jsonify(response), 200
+    
+@constdf_bp.route('/updateconstdf', methods=['POST'])
+@login_required
+def constdf_update_constdf_csv(current_user):
+    if request.method == 'POST':
+        constdf_json = []
+        try:
+            with open(file_path, newline='') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    item = row['texto']
+                    text = item.split('.')
+                    articulo = text[0]
+                    text = articulo.split(' ')
+                    num = int(text[-1])
+                    article = articulo[:-len(text[-1])]
+                    texto = item[len(articulo)+2:]
+                    if 'transitorio' in article:
+                        article = 'Articulo Transitorio'
+                    else:
+                        article = 'Articulo'
+                    constdf_json.append({
+                        'num': num,
+                        'articulo': article,
+                        'texto': texto,
+                        'tutela': row['tutela']
+                    })
+            db = get_db()
+            constdf_collection = db['constdf']
+            constdf_collection.delete_many({})
+            for each in constdf_json:
+                constdf_collection.insert_one(each)
+            response = {
+                'message' : "successfully updated"
+            }
+            return jsonify(response), 200
+        except Exception as e:
+            response = {
+                'message' : "error"
+            }
+            return jsonify(response), 500
+        
+@constdf_bp.route('/uploadconstdfdelete', methods=['POST'])
+@login_required
+def constdf_delete_constdf_csv(current_user):
+    if request.method == 'POST':
+        if os.path.exists(file_path):
+            os.remove(file_path)
--- app/admin/static/js/admin/constdf.js
+++ app/admin/static/js/admin/constdf.js
@@ -1,128 +1,194 @@
-let const_table
+let const_table;
 
-function editConstDf (order) {
-  var rowData = const_table.row(order - 1).data()
-  $('#add_const_model #_id')[0].innerHTML = rowData._id
-  $('#add_const_model #art_type')[0].value = rowData.articulo.includes(
-    'Transitorio'
+function editConstDf(order) {
+  var rowData = const_table.row(order - 1).data();
+  $("#add_const_model #_id")[0].innerHTML = rowData._id;
+  $("#add_const_model #art_type")[0].value = rowData.articulo.includes(
+    "Transitorio"
   )
-    ? 'Articulo Transitorio'
-    : 'Articulo'
+    ? "Articulo Transitorio"
+    : "Articulo";
 
-  $('#add_const_model #articulo')[0].value = rowData.articulo
-    .split(' ')
-    .slice(-1)
-  $('#add_const_model #tutela_type')[0].value = rowData.tutela
-  $('#add_const_model #texto')[0].value = rowData.texto
-  $('#add_const_model').modal('show')
+  $("#add_const_model #articulo")[0].value = rowData.articulo
+    .split(" ")
+    .slice(-1);
+  $("#add_const_model #tutela_type")[0].value = rowData.tutela;
+  $("#add_const_model #texto")[0].value = rowData.texto;
+  $("#add_const_model").modal("show");
 }
 
-function deleteConstDf (order) {
-  var rowData = const_table.row(order - 1).data()
-  $('#delete_const_modal #delete_id')[0].innerHTML = rowData._id
-  $('#delete_const_modal').modal('show')
+function deleteConstDf(order) {
+  var rowData = const_table.row(order - 1).data();
+  $("#delete_const_modal #delete_id")[0].innerHTML = rowData._id;
+  $("#delete_const_modal").modal("show");
 }
 
 $(document).ready(function () {
-  const_table = $('#ConstDf_datatable').DataTable({
+  const_table = $("#ConstDf_datatable").DataTable({
     serverSide: true,
     paging: true,
     initComplete: function (settings, json) {
-      $('#body_preloader').hide() // Hide the loading message when done
+      $("#body_preloader").hide(); // Hide the loading message when done
     },
     preDrawCallback: function (settings) {
-      $('#body_preloader').show() // Show the loading message before drawing the table
+      $("#body_preloader").show(); // Show the loading message before drawing the table
     },
     drawCallback: function (settings) {
-      $('#body_preloader').hide() // Hide the loading message after drawing the table
+      $("#body_preloader").hide(); // Hide the loading message after drawing the table
     },
     ajax: {
-      url: 'get',
-      type: 'POST'
+      url: "get",
+      type: "POST",
     },
-    order: [[1, 'desc']],
+    order: [[1, "desc"]],
     columnDefs: [
       { orderable: false, targets: 0 },
       { orderable: true, targets: 1 },
       { orderable: true, targets: 2 },
       { orderable: true, targets: 3 },
-      { orderable: false, targets: 4 }
+      { orderable: false, targets: 4 },
     ],
     columns: [
-      { data: 'order' },
-      { data: 'articulo' },
-      { data: 'texto' },
-      { data: 'tutela' },
+      { data: "order" },
+      { data: "articulo" },
+      { data: "texto" },
+      { data: "tutela" },
       {
-        data: '_id',
+        data: "_id",
         render: function (data, type, row) {
           return `<div>
           <button type="button" class="btn btn-warning" onclick="editConstDf(${row.order})">Edit</button>
           <button type='button' class='btn btn-danger' onclick="deleteConstDf(${row.order})">Delete</button>
-          </div>`
-        }
-      }
-    ]
-  })
+          </div>`;
+        },
+      },
+    ],
+  });
 
-  $('#add_const').on('click', function () {
-    $('#add_const_model #_id')[0].innerHTML = ''
-    $('#add_const_model #art_type')[0].value = 'Articulo'
-    $('#add_const_model #articulo')[0].value = ''
-    $('#add_const_model #tutela_type')[0].value = 'no'
-    $('#add_const_model #texto')[0].value = ''
-    $('#add_const_model').modal('show')
-  })
+  $("#add_const").on("click", function () {
+    $("#add_const_model #_id")[0].innerHTML = "";
+    $("#add_const_model #art_type")[0].value = "Articulo";
+    $("#add_const_model #articulo")[0].value = "";
+    $("#add_const_model #tutela_type")[0].value = "no";
+    $("#add_const_model #texto")[0].value = "";
+    $("#add_const_model").modal("show");
+  });
 
-  $('#const_save').on('click', function () {
-    const id = $('#add_const_model #_id')[0].innerHTML
-    const type = $('#add_const_model #art_type')[0].value
-    const number = $('#add_const_model #articulo')[0].value
-    const texto = $('#add_const_model #texto')[0].value
-    const tutela = $('#add_const_model #tutela_type')[0].value
-    const page_info = const_table.page.info()
+  $("#const_save").on("click", function () {
+    const id = $("#add_const_model #_id")[0].innerHTML;
+    const type = $("#add_const_model #art_type")[0].value;
+    const number = $("#add_const_model #articulo")[0].value;
+    const texto = $("#add_const_model #texto")[0].value;
+    const tutela = $("#add_const_model #tutela_type")[0].value;
+    const page_info = const_table.page.info();
 
     $.ajax({
-      url: 'save',
-      type: 'POST',
+      url: "save",
+      type: "POST",
       data: {
         id,
         type,
         number,
         texto,
-        tutela
+        tutela,
       },
       success: function (response) {
-        showToast('Saved successfully!')
-        $('.bd-example-modal-lg').modal('hide')
-        const_table.page(page_info.page).draw('page')
+        showToast("Saved successfully!");
+        $(".bd-example-modal-lg").modal("hide");
+        const_table.page(page_info.page).draw("page");
       },
       error: function (xhr, status, error) {
-        showToast('Error: ' + xhr.responseText)
-        $('.bd-example-modal-lg').modal('hide')
-      }
-    })
-  })
+        showToast("Error: " + xhr.responseText);
+        $(".bd-example-modal-lg").modal("hide");
+      },
+    });
+  });
 
-  $('#const_delete').on('click', function () {
-    const id = $('#delete_const_modal #delete_id')[0].innerHTML
-    const page_info = const_table.page.info()
+  $("#const_delete").on("click", function () {
+    const id = $("#delete_const_modal #delete_id")[0].innerHTML;
+    const page_info = const_table.page.info();
 
     $.ajax({
-      url: 'delete',
-      type: 'POST',
+      url: "delete",
+      type: "POST",
       data: {
-        id
+        id,
       },
       success: function (response) {
-        showToast('Deleted successfully!')
-        $('#delete_const_modal').modal('hide')
-        const_table.page(page_info.page).draw('page')
+        showToast("Deleted successfully!");
+        $("#delete_const_modal").modal("hide");
+        const_table.page(page_info.page).draw("page");
       },
       error: function (xhr, status, error) {
-        showToast('Error: ' + xhr.responseText)
-        $('#delete_const_modal').modal('hide')
-      }
-    })
-  })
-})
+        showToast("Error: " + xhr.responseText);
+        $("#delete_const_modal").modal("hide");
+      },
+    });
+  });
+
+  $("#importdf_csv").on("click", function () {
+    $("#import_csv_const_model #constdf_csv_file")[0].value = "";
+    $("#import_csv_const_model").modal("show");
+    $("#import_csv_const_model_preloader").hide();
+  });
+
+  $("#constdf_csv_file").on("change", function (event) {
+    var form_data = new FormData($("#csv_file_upload_form")[0]);
+    if ($("#import_csv_const_model #constdf_csv_file")[0].value == "") return;
+
+    return $.ajax({
+      type: "POST",
+      url: "uploadconstdfcsv",
+      data: form_data,
+      contentType: false,
+      cache: false,
+      processData: false,
+      beforeSend: function () {
+        $("#import_csv_const_model_preloader").show();
+      },
+      success: function (response) {
+        $("#import_csv_const_model_preloader").hide();
+      },
+      statusCode: {
+        401: function () {
+          window.location.href = "/login";
+        },
+      },
+      error: function (xhr, status, error) {
+        showToast("Something went wrong", "danger");
+        console.error("Error occur:", status, error);
+      },
+      complete: function () {
+        $("#import_csv_const_model_preloader").hide();
+      },
+    });
+  });
+
+  $("#constdf_upload_confirm_btn").on("click", function () {
+    return $.ajax({
+      url: "updateconstdf",
+      type: "POST",
+      beforeSend: function () {
+        $("#import_csv_const_model_preloader").show();
+      },
+      success: function (response) {
+        showToast("Updated successfully!");
+        $("#import_csv_const_model").modal("hide");
+        const_table.draw();
+      },
+      error: function (xhr, status, error) {
+        showToast("Error: " + xhr.responseText);
+      },
+      complete: function () {
+        $("#import_csv_const_model_preloader").hide();
+      },
+    });
+  });
+
+  $("#import_csv_const_model").on("hidden.bs.modal", function () {
+    return $.ajax({
+      url: "uploadconstdfdelete",
+      type: "POST",
+    });
+  });
+});
